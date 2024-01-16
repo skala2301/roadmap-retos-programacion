258bb172838e2e94504ac6b37c6a3abcf00ca3dd
Carlos Iglesias <krlos_2301@hotmail.com>
2024-01-15 21:15:38 -0600

    added reto 01 javascript update

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/skala2301.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/skala2301.js
@@ -5,10 +5,10 @@ showArith();
 showLogic();
 showBits();
 showAssign();
-
 showValRetChain();
 showDestruct();
 showComp();
+showEspecialOp();
 
 
 /******
@@ -115,10 +115,7 @@ function showAssign(){
     console.log("donde\nbooleanA: ",booleanA,"\nbooleanB: ",booleanB);
     booleanA ??= booleanB;       //asignacion de anulacion logica, booleanA adquiere valor de booleanB solo si booleanA es null o undefined
     console.log("Para operacion booleanA ??= booleanB --> booleanA mantiene el valor: ",booleanA);
-    console.log("\n");
-
-
-    
+    console.log("\n"); 
 }
 
 function showArith(){
@@ -259,7 +256,6 @@ function showLogic(){
     result = booleanA && booleanB;       //AND Logico
     console.log("Para operacion booleanA && booleanB --> el resultado es: ",result);
     console.log("\n");
-
     
     console.log("operador || (OR LOGICO)");
     console.log("donde\nbooleanA: ",booleanA,"\nbooleanB: ",booleanB);
@@ -294,15 +290,11 @@ function showValRetChain(){
     console.log("result = A = B")
     console.log("result: ",result,"\n\n");
     
-    
-
     console.log("La expresion A *= B TIENE UN VALOR DE RETORNO QUE COINCIDE CON EL RESULTADO DE A * B, ESTA LOGICA SE REPITE PARA TODAS LAS OPERACIONES DE ASIGNACION");
     console.log("donde\nA: ",A,"\nB: ",B);
     result = A *= B;
     console.log("result = A *= B")
     console.log("result: ",result,"\n\n");
-
-
 
 }
 
@@ -352,10 +344,7 @@ function showComp(){
     let numA = 2;       
     let numB = 5; 
     let strA = "2.0";
-    let strB = "5.0";
     let result = 0;
-    let booleanA = true;
-    let booleanB = false;
 
     console.log("operador == (igualdad)"); 
     console.log("donde\n numA: ",numA,"\nstrA: ",strA);     
@@ -411,6 +400,44 @@ function showComp(){
     console.log("donde\n numA: ",numA,"\nnumB: ",numB);     
     result = numA <= numB;       //menor o igual que
     console.log("Para operacion numA <= numB --> el resultado es: ",result,"\n\n");
+
+    console.log("operador in");    
+    console.log("El operador in devuelve true si la propiedad especificada está en el objeto especificado o su prototipo.\n\
+'yo' in {tu: 'tu',yo: 'yo',ellos: 'ellos'} = ",'yo' in {tu: 'tu',yo: 'yo',ellos: 'ellos'},"\n\n");
+
+    console.log("operador instanceof"); 
+    strA = new String("Cuento");   
+    console.log("El operador instanceof verifica si un objeto en su cadena de prototipos contiene la propiedad prototype de un constructor.\n\
+se tiene strA = String('Cuento'); verificamos si strA es instancia de String de la siguiente forma:\n\
+strA instanceof String = ",strA instanceof String,"\n\n");
+
 }
+
+function showEspecialOp(){
+    console.log("EVALUACION DE CORTOCIRCUITO");
+    let booleanResult=undefined;
+    let value = "Something in the wind";
+    console.log("las expresiones logicas se evaluan de izquierda a derecha\n\
+por lo que expresiones como la siguiente: \nbooleanResult = false && true;\n\
+se evaluan en cortocircuito, solo se evalua el primer resultado (false para &&) y se ignora el resto");
+    booleanResult = false && true;
+    console.log("en este ejemplo el valor de booleanResult = ", booleanResult);
+    console.log("por otro lado booleanResult = true || false;\n\
+se evaluan en cortocircuito, solo se evalua el primer resultado (true para ||) y se ignora el resto");
+    booleanResult = true || false;
+    console.log("en este ejemplo el valor de booleanResult = ", booleanResult);
+    console.log("\n");
+    console.log("OPERADOR TERNARIO");
+    console.log("Para evaluacion de expresiones logicas se dispone tambien de el operador ternario ?\n\
+como ejemplo: booleanResult = value ? value : null; \n\
+evalua si la condicion en value es verdadero, si lo es se ejecuta la expresion antes de : de lo contrario se ejecuta\n\
+la expresion despues de :");
+    booleanResult = value ? value : null;
+    console.log("en este ejemplo:\nvalue:",value,
+    "\nel valor de booleanResult = ", booleanResult);
+}
+
+
+
 
 
